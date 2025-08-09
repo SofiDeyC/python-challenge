4a8ed125e80f91e461e344b4122005f20d133158
Sofia Dey Choudhury <123995081+SofiDeyC@users.noreply.github.com>
2025-08-09 11:51:00 +0100

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,6 +1,6 @@
 name = "Sofia"
-age = 16
+age = 14
 hobby = "playing guitar"
 
 print("Hi! My name is", name, "and I am", age, "years old.")
-print("My favourite hobby is", hobby)
+print("My favourite hobby is " + hobby)
