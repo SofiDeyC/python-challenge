a07e67797f5a05edf40a388ca126942357801373
Sofia Dey Choudhury <123995081+SofiDeyC@users.noreply.github.com>
2025-08-09 12:12:58 +0100

    Update day3.py

--- day3.py
+++ day3.py
@@ -1,8 +1,3 @@
 # simple count
 for i in range(1, 11):
     print(i)
-
-# loop over a list (example: SDGs)
-sdgs = ["No Poverty", "Quality Education", "Climate Action"]
-for goal in sdgs:
-    print("SDG:", goal)
