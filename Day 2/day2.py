d1181d950572bbc0fc2d240cec9488055b2c278d
Sofia Dey Choudhury <123995081+sofia-dc@users.noreply.github.com>
2025-08-21 00:52:57 +0100

    Delete Day 2 directory

--- Day 2/day2.py
+++
@@ -1,8 +0,0 @@
-age = 16
-
-if age >= 18:
-    print("You can vote!")
-elif age >= 13:
-    print("You're a teen — keep learning and coding!")
-else:
-    print("You're young and brilliant — lots of time to grow")
