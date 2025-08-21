3d6a48c514aa2a3509afe567fdd15af735150de2
Sofia Dey Choudhury <123995081+sofia-dc@users.noreply.github.com>
2025-08-21 01:08:28 +0100

    Create day5.py

---
+++ day5.py
@@ -0,0 +1,13 @@
+# A list of SDGs
+sdgs = ["No Poverty", "Zero Hunger", "Quality Education"]
+
+# Print one goal by index
+print(sdgs[1])  # Prints "Zero Hunger"
+
+# Update the second goal
+sdgs[1] = "Good Health"
+print(sdgs)
+
+# A tuple of fixed values
+sdgs1 = ("Peace", "Justice", "Partnerships")
+print(sdgs1[0])  # Prints "Peace"
