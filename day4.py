fac896cdd82f1daef49f7f75700d70ccfb438c4d
Sofia Dey Choudhury <123995081+sofia-dc@users.noreply.github.com>
2025-08-21 01:07:45 +0100

    Create day4.py

---
+++ day4.py
@@ -0,0 +1,15 @@
+# Define a function to grade a test score
+def grade(score):
+    if score >= 50:
+        print("pass")
+    else:
+        print("fail")
+
+# Call the function
+print(grade(82))
+
+# A compliment generator function
+def compliment(name):
+  print("Hey", name, "you look great today!")
+
+compliment("Sofia")
