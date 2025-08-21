8774b56d0df85d423d21da75e4ad8d3c6a3b874b
Sofia Dey Choudhury <123995081+sofia-dc@users.noreply.github.com>
2025-08-21 01:03:50 +0100

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,16 @@
+# Store age in a variable
+age = 14
+
+# Check voting eligibility
+if age >= 18:
+    print("You can vote.")
+else:
+    print("You can't vote.")
+
+# School stage classification
+if age < 16:
+  print("Secondary school")
+elif age < 18:
+  print("Sixth form")
+else:
+  print("University or working")
