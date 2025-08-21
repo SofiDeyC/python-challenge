d52dfdfd52e1895ebcb78fc0a38ffcabe426c446
Sofia Dey Choudhury <123995081+sofia-dc@users.noreply.github.com>
2025-08-21 01:09:31 +0100

    Create day6.py

---
+++ day6.py
@@ -0,0 +1,13 @@
+# A dictionary for a user profile
+user = {
+    "name": "Sofia",
+    "age": 14,
+    "location": "London"
+}
+
+# Access values using keys
+print(user["name"])   # Prints Sofia
+print(user["age"])    # Prints 14
+
+user["name"] = "Sophie"
+print(user)
