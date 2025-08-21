9c0832d22c76bf52ddf027205d91d47d698eee54
Sofia Dey Choudhury <123995081+sofia-dc@users.noreply.github.com>
2025-08-21 01:05:11 +0100

    Create day3.py

---
+++ day3.py
@@ -0,0 +1,10 @@
+# Count from 1 to 10
+for i in range(1, 11):
+    print(i)
+
+# List of a few SDGs
+sdgs = ["No Poverty", "Zero Hunger", "Quality Education"]
+
+# Loop through the list
+for goal in sdgs:
+    print("Goal:", goal)
